6f21da367c00edccf44c2e6518a5a2059cd78dee
adigibli <gibliadi@gmail.com>
2020-09-30 08:46:00 +0300

    all project

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,19 @@
+import { Recipe } from './../Recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A test Recipe", "This is the sample recipe", "https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?fit=1200%2C879&ssl=1")
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
